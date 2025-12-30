615907e38f797303c11761dd5d8067c2c8c80a6e
Pedro Suedam <ptsuedam@gmail.com>
2025-12-30 00:10:06 -0300

    desenvolvimento da tela de info

--- javaScript/info.js
+++ javaScript/info.js
@@ -3,20 +3,30 @@ const botao2 = document.querySelector(".bnt2");
 const botao3 = document.querySelector(".bnt3");
 const botao4 = document.querySelector(".bnt4");
 
-var texto = document.getElementById("text");
+const texto = document.querySelector(".text");
+
+var text1 = "como funciona"
+var text2 = "indicações"
+var text3 = "cuidados"
+var text4 = "perguntas"
 
 botao1.addEventListener("click", () => {
-    texto.textContent = "como funciona"
+    texto.textContent = text1
 })
 
 botao2.addEventListener("click", () => {
-    texto.textContent = "indicações"
+    texto.textContent = text2
 })
 
 botao3.addEventListener("click", () => {
-    texto.textContent = "cuidados"
+    texto.textContent = text3
 })
 
 botao4.addEventListener("click", () => {
-    texto.textContent = "perguntas"
+    texto.textContent = text4
 })
+
+if (texto.textContent === "como funciona") {
+  botao1.classList.add("link-verde");
+}
+
