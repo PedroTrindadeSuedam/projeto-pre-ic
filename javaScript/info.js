2ae0cc5fedd87a3a39606800d968236b0269c6df
Pedro Suedam <ptsuedam@gmail.com>
2026-01-24 22:01:06 -0300

    desenvolvimento da tela de info

--- javaScript/info.js
+++ javaScript/info.js
@@ -12,21 +12,38 @@ var text4 = "perguntas"
 
 botao1.addEventListener("click", () => {
     texto.textContent = text1
+    botao1.classList.add("bnt-ativo")
+
+    botao2.classList.remove("bnt-ativo")
+    botao3.classList.remove("bnt-ativo")
+    botao4.classList.remove("bnt-ativo")
+
 })
 
 botao2.addEventListener("click", () => {
     texto.textContent = text2
+    botao2.classList.add("bnt-ativo")
+
+    botao1.classList.remove("bnt-ativo")
+    botao3.classList.remove("bnt-ativo")
+    botao4.classList.remove("bnt-ativo")
 })
 
 botao3.addEventListener("click", () => {
     texto.textContent = text3
+    botao3.classList.add("bnt-ativo")
+
+    botao1.classList.remove("bnt-ativo")
+    botao2.classList.remove("bnt-ativo")
+    botao4.classList.remove("bnt-ativo")
 })
 
 botao4.addEventListener("click", () => {
     texto.textContent = text4
-})
+    botao4.classList.add("bnt-ativo")
 
-if (texto.textContent === "como funciona") {
-  botao1.classList.add("link-verde");
-}
+    botao1.classList.remove("bnt-ativo")
+    botao2.classList.remove("bnt-ativo")
+    botao3.classList.remove("bnt-ativo")
+})
 
