3424d45203414483002ef78b5ee5b6f82348eff6
Pedro Suedam <ptsuedam@gmail.com>
2026-03-23 21:21:10 -0300

    desenvolvimento da tela de midea do aparelho

--- javaScript/info.js
+++ javaScript/info.js
@@ -5,19 +5,20 @@ const botao4 = document.querySelector(".bnt4");
 
 const texto = document.querySelector(".text");
 
-var text1 = "como funciona"
+var text1 = "A expansão rápida da maxila (ERM) é o procedimento ortopédico mais utilizado para corrigir a atresia maxilar e a mordida cruzada posterior. A intervenção precoce, ainda na dentadura decídua ou mista, é recomendada para corrigir a deficiência transversal. Os aparelhos expansores do tipo Haas e Hyrax promovem o aumento da largura da arcada superior por meio da ativação de um parafuso localizado no centro do palato, resultando na ampliação das distâncias intermolares e intercaninas."
+
 var text2 = "indicações"
 var text3 = "cuidados"
 var text4 = "perguntas"
 
 botao1.addEventListener("click", () => {
     texto.textContent = text1
+   
     botao1.classList.add("bnt-ativo")
 
     botao2.classList.remove("bnt-ativo")
     botao3.classList.remove("bnt-ativo")
     botao4.classList.remove("bnt-ativo")
-
 })
 
 botao2.addEventListener("click", () => {
@@ -46,4 +47,3 @@ botao4.addEventListener("click", () => {
     botao2.classList.remove("bnt-ativo")
     botao3.classList.remove("bnt-ativo")
 })
-
